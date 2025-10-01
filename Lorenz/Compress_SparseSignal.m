de4b1695537cf6aad8b835e88e73073b65934f4c
Ngan Nhu <ngannhu0305@gmail.com>
2025-10-01 11:50:21 +0700

    01102025 v1

---
+++ Lorenz/Compress_SparseSignal.m
@@ -0,0 +1,59 @@
+% Author: Uyen L.P.Nguyen
+% Observer 
+% Sparse signal
+
+close all; clear all; clc;
+
+%% Define parameters
+N=512; % signal length
+K=20; % sparsity of signal
+M=6*K; % the number of measurements 
+
+%% Construction of K-sparse signal
+x=zeros(N,1); 
+x(randperm(N,K))=randn(K,1);
+x_sparse = x;
+
+%% Sensing matrix construction
+phi=randn(M,N);
+
+%% Sensing using CS 
+y=phi*x;
+y_cp =y;
+save('y_measurement400.mat','phi','y_cp','x_sparse')
+
+%% l1-recovery using linear program
+% 
+% % transfering l1 minimization into linear program
+% Vec_ones = ones([2 * N, 1]);
+% Vec_low = zeros([2 * N, 1]);
+% Vec_high = inf([2 * N, 1]);
+% 
+% ssOpt=optimoptions('linprog', 'Algorithm', 'interior-point');
+% tic
+% z_hat=linprog(Vec_ones,[],[], [phi -phi], y, Vec_low, Vec_high,ssOpt);
+% toc
+% x_hat=z_hat(1:N)-z_hat(1+N:end);
+% 
+% figure;
+% plot(x);
+% hold on;
+% plot((x_hat), 'r.');
+% legend('Original', 'Recovered');
+% 
+% disp("Mean square error is")
+% mse(x,x_hat)
+
+%% Plot
+figure;
+subplot(1,2,1);
+stem(x_sparse,'filled');
+title('K-sparse signal');
+xlabel('Index'); ylabel('Amplitude');
+grid on;
+
+subplot(1,2,2);
+imagesc(abs(x_sparse)); 
+colormap('hot'); colorbar;
+title('Heatmap of sparse signal');
+
